b7f9ea7e01c63e145ad4e9aed55b6e1d2e0e4a1a
urexpect <106760141+urexpect@users.noreply.github.com>
2022-06-07 07:33:58 +0300

    Create twitch.py

---
+++ twitch.py
@@ -0,0 +1,67 @@
+SendWebhook(self,title,message,icon_url,thumbnail_url,proxy,useragent):
+        try:
+            timestamp = str(datetime.utcnow())
+
+            message_to_send = {"embeds": [{"title": title,"description": message,"color": 65362,"footer": {"text": "MADE BY The uRexPecT#9993 ","icon_url": icon_url},"thumbnail": {"url": thumbnail_url},"timestamp": timestamp}]}
+            
+            headers = {
+                'User-Agent':useragent,
+                'Pragma':'no-cache',
+                'Accept':'*/*',
+                'Content-Type':'application/json'
+            }
+
+            payload = json.dumps(message_to_send)
+
+            if self.use_proxy == 1:
+                response = requests.post(self.webhook_url,data=payload,headers=headers,proxies=proxy)
+            else:
+                response = requests.post(self.webhook_url,data=payload,headers=headers)
+
+            if response.text == "":
+                pass
+            elif "You are being rate limited." in response.text:
+                self.webhook_retries += 1
+                self.SendWebhook(title,message,icon_url,thumbnail_url,proxy,useragent)
+            else:
+                self.webhook_retries += 1
+                self.SendWebhook(title,message,icon_url,thumbnail_url,proxy,useragent)
+        except:
+            self.webhook_retries += 1
+            self.SendWebhook(title,message,icon_url,thumbnail_url,proxy,useragent)
+
+    def GetRandomUserAgent(self):
+        useragents = self.ReadFile('[Data]/useragents.txt','r')
+        return choice(useragents)
+
+    def PrintText(self,bracket_color:Fore,text_in_bracket_color:Fore,text_in_bracket,text):
+        self.lock.acquire()
+        stdout.flush()
+        text = text.encode('ascii','replace').decode()
+        stdout.write(Style.BRIGHT+bracket_color+'['+text_in_bracket_color+text_in_bracket+bracket_color+'] '+bracket_color+text+'\n')
+        self.lock.release()
+
+    def GetRandomProxy(self):
+        proxies_file = self.ReadFile('[Data]/proxies.txt','r')
+        proxies = {}
+        if self.proxy_type == 1:
+            proxies = {
+                "http":"http://{0}".format(choice(proxies_file)),
+                "https":"https://{0}".format(choice(proxies_file))
+            }
+        elif self.proxy_type == 2:
+            proxies = {
+                "http":"socks4://{0}".format(choice(proxies_file)),
+                "https":"socks4://{0}".format(choice(proxies_file))
+            }
+        else:
+            proxies = {
+                "http":"socks5://{0}".format(choice(proxies_file)),
+                "https":"socks5://{0}".format(choice(proxies_file))
+            }
+        return proxies
+
+    def TitleUpdate(self):
+        while True:
+            self.SetTitle(f'[uRexPecT#9993 Twitch Token Checker] ^| HITS: {self.hits} ^| BADS: {self.bads} ^| RETRIES: {self.retries} ^| WEBHOOK RETRIES: {self.webhook_retries} ^| THREADS: {active_count()-1}')
+            sleep(0.1)
